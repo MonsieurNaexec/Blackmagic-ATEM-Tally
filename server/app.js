ff56c6f65072c532eb32ab78c958f359a982a171
MonsieurNaexec <naexec@m-naexec.be>
2024-04-19 12:41:44 +0200

    formatting

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-const {Atem, AtemStateUtil} = require("atem-connection");
+const { Atem, AtemStateUtil } = require("atem-connection");
 var nconf = require("nconf");
-var udp = require('dgram');
-var buffer = require('buffer');
-var client = udp.createSocket('udp4');
+var udp = require("dgram");
+var buffer = require("buffer");
+var client = udp.createSocket("udp4");
 var atemIP = undefined;
 const atem = new Atem();
-var server = udp.createSocket('udp4');
+var server = udp.createSocket("udp4");
 var incomingPort = 8001;
 var outgoingPort = 5657;
 
@@ -14,194 +14,231 @@ var tallyDevices = {};
 
 //Attempt to load in the configuration
 function loadConfig(callback) {
-    nconf.use("file", {file: "./config.json"});
-    nconf.load();
+  nconf.use("file", { file: "./config.json" });
+  nconf.load();
 
-    var error = false;
-    if(nconf.get("atemIP") === undefined) {nconf.set("atemIP", "127.0.0.1"); error = true;}
+  var error = false;
+  if (nconf.get("atemIP") === undefined) {
+    nconf.set("atemIP", "127.0.0.1");
+    error = true;
+  }
 
-    if(error) {
-        //Error
-        console.log("There is an issue with the configuration file. Please check the configuration file config.json");
-        
-        nconf.save(function (error) {
-            if(error){console.log("An error occurred saving the config file: " + error.message);}
-            callback(false);
-        });
-    }
-    else {
-        //Load in the settings
-        atemIP = nconf.get("atemIP");
-        callback(true);
-    }
+  if (error) {
+    //Error
+    console.log(
+      "There is an issue with the configuration file. Please check the configuration file config.json"
+    );
+
+    nconf.save(function (error) {
+      if (error) {
+        console.log(
+          "An error occurred saving the config file: " + error.message
+        );
+      }
+      callback(false);
+    });
+  } else {
+    //Load in the settings
+    atemIP = nconf.get("atemIP");
+    callback(true);
+  }
 }
 
 //Send out a message to a device
 function sendMessage(buffer, ip) {
-    client.send(buffer, outgoingPort, ip, (error) => {
-        if(error) {
-            console.log("Error sending packet to tally @ " + ip);
-        }
-    });
+  client.send(buffer, outgoingPort, ip, (error) => {
+    if (error) {
+      console.log("Error sending packet to tally @ " + ip);
+    }
+  });
 }
 
 //Send a ping response
 function sendPing(ip) {
-    sendMessage(new Buffer.from([0xFA]), ip);
+  sendMessage(new Buffer.from([0xfa]), ip);
 }
 
 //Send out the tally data for the basic tally
-function sendTallyData(forceSend=false) {
-    for(var i in tallyDevices) {
-        var isProg = false;
-        var isPrev = false;
-        var isDSKey = false;
-        var isUSKey = false;
+function sendTallyData(forceSend = false) {
+  for (var i in tallyDevices) {
+    var isProg = false;
+    var isPrev = false;
+    var isDSKey = false;
+    var isUSKey = false;
 
-        //Downstream keyers
-        for(var j in atem.state.video.downstreamKeyers) {
-            if((atem.state.video.downstreamKeyers[j].sources.fillSource == tallyDevices[i].inputId || atem.state.video.downstreamKeyers[j].sources.cutSource == tallyDevices[i].inputId) && atem.state.video.downstreamKeyers[j].onAir) {
-                isDSKey = true;
-                break;
-            }
-        }
-        
-        //Check MEs
-        for(var j in atem.state.video.ME) {
-            if(!tallyDevices[i].ignoredMEs.includes(j)) {
-                //Check US keyer
-                for(var k in atem.state.video.ME[j].upstreamKeyers) {
-                    if((atem.state.video.ME[j].upstreamKeyers[k].fillSource == tallyDevices[i].inputId || atem.state.video.ME[j].upstreamKeyers[k].cutSource  == tallyDevices[i].inputId) && atem.state.video.ME[j].upstreamKeyers[k].onAir) {
-                        isUSKey = true;
-                        break;
-                    }
-                }
-
-                if(!isProg) {isProg = atem.state.video.ME[j].programInput == tallyDevices[i].inputId || atem.state.video.ME[j].inTransition;}
-                if(!isPrev) {isPrev = atem.state.video.ME[j].previewInput == tallyDevices[i].inputId;}
-            }
-        }
-
-        //Prog
-        if(tallyDevices[i].isProg != isProg || forceSend) {
-            tallyDevices[i].isProg = isProg;
-            sendMessage(Buffer.from([0x01, isProg, 0x00]), i);
-        }
-        //Prev
-        if(tallyDevices[i].isPrev != isPrev || forceSend) {
-            tallyDevices[i].isPrev = isPrev;
-            sendMessage(Buffer.from([0x02, isPrev, 0x00]), i);
-        }
-        //DS Key
-        if(tallyDevices[i].isDSKey != isDSKey || forceSend) {
-            tallyDevices[i].isDSKey = isDSKey;
-            sendMessage(Buffer.from([0x03, isDSKey, 0x00]), i);
-        }
-        //US Key
-        if(tallyDevices[i].isUSKey != isUSKey || forceSend) {
-            tallyDevices[i].isUSKey = isUSKey;
-            sendMessage(Buffer.from([0x04, isUSKey, 0x00]), i);
-        }
+    //Downstream keyers
+    for (var j in atem.state.video.downstreamKeyers) {
+      if (
+        (atem.state.video.downstreamKeyers[j].sources.fillSource ==
+          tallyDevices[i].inputId ||
+          atem.state.video.downstreamKeyers[j].sources.cutSource ==
+            tallyDevices[i].inputId) &&
+        atem.state.video.downstreamKeyers[j].onAir
+      ) {
+        isDSKey = true;
+        break;
+      }
     }
+
+    //Check MEs
+    for (var j in atem.state.video.ME) {
+      if (!tallyDevices[i].ignoredMEs.includes(j)) {
+        //Check US keyer
+        for (var k in atem.state.video.ME[j].upstreamKeyers) {
+          if (
+            (atem.state.video.ME[j].upstreamKeyers[k].fillSource ==
+              tallyDevices[i].inputId ||
+              atem.state.video.ME[j].upstreamKeyers[k].cutSource ==
+                tallyDevices[i].inputId) &&
+            atem.state.video.ME[j].upstreamKeyers[k].onAir
+          ) {
+            isUSKey = true;
+            break;
+          }
+        }
+
+        if (!isProg) {
+          isProg =
+            atem.state.video.ME[j].programInput == tallyDevices[i].inputId ||
+            atem.state.video.ME[j].inTransition;
+        }
+        if (!isPrev) {
+          isPrev =
+            atem.state.video.ME[j].previewInput == tallyDevices[i].inputId;
+        }
+      }
+    }
+
+    //Prog
+    if (tallyDevices[i].isProg != isProg || forceSend) {
+      tallyDevices[i].isProg = isProg;
+      sendMessage(Buffer.from([0x01, isProg, 0x00]), i);
+    }
+    //Prev
+    if (tallyDevices[i].isPrev != isPrev || forceSend) {
+      tallyDevices[i].isPrev = isPrev;
+      sendMessage(Buffer.from([0x02, isPrev, 0x00]), i);
+    }
+    //DS Key
+    if (tallyDevices[i].isDSKey != isDSKey || forceSend) {
+      tallyDevices[i].isDSKey = isDSKey;
+      sendMessage(Buffer.from([0x03, isDSKey, 0x00]), i);
+    }
+    //US Key
+    if (tallyDevices[i].isUSKey != isUSKey || forceSend) {
+      tallyDevices[i].isUSKey = isUSKey;
+      sendMessage(Buffer.from([0x04, isUSKey, 0x00]), i);
+    }
+  }
 }
 
 //Send out the CCU data
-function sendCCUData() {
-}
+function sendCCUData() {}
 
 //Connect to the ATEM
 function connect() {
-    var self = this;
+  var self = this;
 
-    //Set the handlers
-    atem.on("info", function(message) {
-        console.log("INFO: " + message);
+  //Set the handlers
+  atem.on("info", function (message) {
+    console.log("INFO: " + message);
+  });
+  atem.on("error", function (error) {
+    console.log("ERROR: " + error);
+  });
+
+  atem.on("connected", () => {
+    console.log("Successfully connected to the ATEM");
+    sendTallyData();
+    sendCCUData();
+
+    atem.on("stateChanged", (state, pathToChange) => {
+      console.log("ATEM State changed");
+      sendTallyData();
+      sendCCUData();
     });
-    atem.on("error", function(error) {
-        console.log("ERROR: " + error);
-    });
+  });
 
-    atem.on("connected", () => {
-        console.log("Successfully connected to the ATEM");
-        sendTallyData();
-        sendCCUData();
+  atem.on("disconnected", function () {
+    console.log("Disconnected from the ATEM");
+  });
 
-        atem.on('stateChanged', (state, pathToChange) => {
-            sendTallyData();
-            sendCCUData();
-        });      
-    });
+  server.on("error", (error) => {
+    console.log("A tally server error occurred: " + error);
+  });
 
-    atem.on("disconnected", function() {
-        console.log("Disconnected from the ATEM");
-    });
+  server.on("listening", (error) => {
+    console.log(
+      "The tally server is listening on " +
+        server.address().address +
+        ":" +
+        server.address().port
+    );
+  });
 
-    server.on("error", (error) => {
-        console.log("A tally server error occurred: " + error);
-    });
-
-    server.on("listening", (error) => {
-        console.log("The tally server is listening on " + server.address().address + ":" + server.address().port);
-    });
-
-    server.on("message", (msg, info) => {
-        switch(msg[0]) {
-            //Subscribe request for tally function
-            case 0xAF: {
-                var ignoredMEs = [];
-                var ignoredMEsFriendly = "";
-                for(var i = 2; i < msg.length; i++) {
-                    ignoredMEs[i - 2] = msg[i];
-                    ignoredMEsFriendly += msg[i] + ",";
-                }
-                console.log("Tally subscribe request from " + info.address + " InputID: " + msg[1] + " Ignored MEs: " + ignoredMEsFriendly.slice(0, -1));
-
-                //Add this device to our pool to send to
-                tallyDevices[info.address] = {
-                    "inputId": msg[1],
-                    "ignoredMEs": ignoredMEs,
-                    "isProg": false,
-                    "isPrev": false,
-                    "isDSKey": false,
-                    "isUSKey": false
-                };
-
-                sendPing(info.address);
-                sendTallyData();
-                break;
-            }
-            //Subscribe request for ccu function
-            case 0xAD: {
-                
-            }
-            //Ping request
-            case 0xFA: {
-                sendPing(info.address);
-                break;
-            }
+  server.on("message", (msg, info) => {
+    switch (msg[0]) {
+      //Subscribe request for tally function
+      case 0xaf: {
+        var ignoredMEs = [];
+        var ignoredMEsFriendly = "";
+        for (var i = 2; i < msg.length; i++) {
+          ignoredMEs[i - 2] = msg[i];
+          ignoredMEsFriendly += msg[i] + ",";
         }
+        console.log(
+          "Tally subscribe request from " +
+            info.address +
+            " InputID: " +
+            msg[1] +
+            " Ignored MEs: " +
+            ignoredMEsFriendly.slice(0, -1)
+        );
 
-        // for(var i = 0; i < msg.length; i++) {
-        //     console.log(msg[i]);
-        // }
-    });
+        //Add this device to our pool to send to
+        tallyDevices[info.address] = {
+          inputId: msg[1],
+          ignoredMEs: ignoredMEs,
+          isProg: false,
+          isPrev: false,
+          isDSKey: false,
+          isUSKey: false,
+        };
 
-    //Connect
-    atem.connect(atemIP);
-    server.bind(incomingPort);
+        sendPing(info.address);
+        sendTallyData();
+        break;
+      }
+      //Subscribe request for ccu function
+      case 0xad: {
+      }
+      //Ping request
+      case 0xfa: {
+        sendPing(info.address);
+        break;
+      }
+    }
+
+    // for(var i = 0; i < msg.length; i++) {
+    //     console.log(msg[i]);
+    // }
+  });
+
+  //Connect
+  atem.connect(atemIP);
+  server.bind(incomingPort);
 }
 
 //Main loop
 console.log("ATEM tally server by Kardinia Church 2021");
 console.log("Attempting to load configuration");
 
-loadConfig(function(success) {
-    if(success == true) {
-        //Everything seems good lets begin!
-        console.log("Success, attempting connection to the ATEM at " + atemIP);
-        connect();
-    }
-    else {
-        console.error("Initialization errors occurred, cannot continue");
-    }
+loadConfig(function (success) {
+  if (success == true) {
+    //Everything seems good lets begin!
+    console.log("Success, attempting connection to the ATEM at " + atemIP);
+    connect();
+  } else {
+    console.error("Initialization errors occurred, cannot continue");
+  }
 });
--- tally/Source/src/main.cpp
+++ tally/Source/src/main.cpp
@@ -13,7 +13,7 @@
 
 #define SERIAL_BAUD 115200
 #define INCOMING_PORT 5657
-#define OUTGOING_PORT 8002
+#define OUTGOING_PORT 8001
 #define DEBUG false
 unsigned long thisMs = 0;
 
@@ -26,7 +26,7 @@ unsigned long thisMs = 0;
 
 WiFiUDP udp;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, 4, NEO_GRB + NEO_KHZ800);
-IPAddress serverIP(255,255,255,255);
+IPAddress serverIP(255, 255, 255, 255);
 unsigned long lastMessage = millis();
 bool programTally = false;
 bool previewTally = false;
@@ -36,13 +36,15 @@ bool pingSent = false;
 
 #include "settings.h"
 
-//Send a request to subscribe
-void subscribe() {
+// Send a request to subscribe
+void subscribe()
+{
   Serial.println("Subscribe");
-  //Subscribe cmd, inputId, ignoreME(s)
+  // Subscribe cmd, inputId, ignoreME(s)
   udp.beginPacket(serverIP, OUTGOING_PORT);
   uint8_t buffer[(sizeof(ignoredMEs) / sizeof(ignoredMEs[0])) + 2] = {CMD_SUBSCRIBE, inputID};
-  for(int i = 0; i < (sizeof(ignoredMEs) / sizeof(ignoredMEs[0])); i++) {
+  for (int i = 0; i < (sizeof(ignoredMEs) / sizeof(ignoredMEs[0])); i++)
+  {
     buffer[i + 2] = ignoredMEs[i];
   }
 
@@ -51,8 +53,9 @@ void subscribe() {
   lastMessage = millis();
 }
 
-//Send a ping request
-void ping() {
+// Send a ping request
+void ping()
+{
   pingSent = true;
   Serial.println("Send ping");
   udp.beginPacket(serverIP, OUTGOING_PORT);
@@ -62,86 +65,110 @@ void ping() {
   lastMessage = millis();
 }
 
-//Process the incoming packet
+// Process the incoming packet
 byte packetBuffer[UDP_TX_PACKET_MAX_SIZE];
-void receivePacket() {
+void receivePacket()
+{
   int packetSize = udp.parsePacket();
-  if (packetSize > 0) {
+  if (packetSize > 0)
+  {
     udp.read(packetBuffer, packetSize);
-    switch(packetBuffer[0]) {
-      case CMD_PROGRAM: {
-        programTally = packetBuffer[1] == 0x01;
-        pingSent = false;
-        lastMessage = millis();
-        break;
-      }
-      case CMD_PREVIEW: {
-        previewTally = packetBuffer[1] == 0x01;
-        pingSent = false;
-        lastMessage = millis();
-        break;
-      }
-      case CMD_DSKEY: {
-        dsKeyTally = packetBuffer[1] == 0x01;
-        pingSent = false;
-        lastMessage = millis();
-        break;
-      }
-      case CMD_USKEY: {
-        usKeyTally = packetBuffer[1] == 0x01;
-        pingSent = false;
-        lastMessage = millis();
-        break;
-      }
-      case CMD_PING: {
-        pingSent = false;
-        lastMessage = millis();
-        Serial.println("RESPONSE");
-        break;
-      }
+    switch (packetBuffer[0])
+    {
+    case CMD_PROGRAM:
+    {
+      programTally = packetBuffer[1] == 0x01;
+      pingSent = false;
+      lastMessage = millis();
+      break;
+    }
+    case CMD_PREVIEW:
+    {
+      previewTally = packetBuffer[1] == 0x01;
+      pingSent = false;
+      lastMessage = millis();
+      break;
+    }
+    case CMD_DSKEY:
+    {
+      dsKeyTally = packetBuffer[1] == 0x01;
+      pingSent = false;
+      lastMessage = millis();
+      break;
+    }
+    case CMD_USKEY:
+    {
+      usKeyTally = packetBuffer[1] == 0x01;
+      pingSent = false;
+      lastMessage = millis();
+      break;
+    }
+    case CMD_PING:
+    {
+      pingSent = false;
+      lastMessage = millis();
+      Serial.println("RESPONSE");
+      break;
+    }
     }
 
-    //Do colours
-    if((dsKeyTally && keyerDSEnabled) || (usKeyTally && keyerUSEnabled)) {
-      //Tally colour
+    // Do colours
+    if ((dsKeyTally && keyerDSEnabled) || (usKeyTally && keyerUSEnabled))
+    {
+      // Tally colour
       strip.setPixelColor(0, keyerColor);
       strip.setPixelColor(1, keyerColor);
     }
-    else if(programTally && programEnabled) {
-      //Program colour
+    else if (programTally && programEnabled)
+    {
+      // Program colour
       strip.setPixelColor(0, liveColor);
       strip.setPixelColor(1, liveColor);
     }
-    else if(previewTally && previewEnabled) {
-      //Preview colour
+    else if (previewTally && previewEnabled)
+    {
+      // Preview colour
       strip.setPixelColor(0, standbyColor);
-      if(showBlue) {strip.setPixelColor(1, blueColor);}else{strip.setPixelColor(1, offColor);}
+      if (showBlue)
+      {
+        strip.setPixelColor(1, blueColor);
+      }
+      else
+      {
+        strip.setPixelColor(1, offColor);
+      }
     }
-    else if(showBlue) {
-      //Blue colour
+    else if (showBlue)
+    {
+      // Blue colour
       strip.setPixelColor(0, offColor);
       strip.setPixelColor(1, blueColor);
     }
-    else {
-      //All off
+    else
+    {
+      // All off
       strip.setPixelColor(0, offColor);
       strip.setPixelColor(1, offColor);
     }
   }
-  else if(lastMessage + 5000 < millis()) {
+  else if (lastMessage + 5000 < millis())
+  {
     Serial.println("CHECK PING");
-    if(pingSent) {
+    if (pingSent)
+    {
       // strip.setPixelColor(0, blueColor);
       // strip.setPixelColor(1, blueColor);
       subscribe();
     }
-    else {
+    else
+    {
       ping();
     }
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(SERIAL_BAUD);
   Serial.println("Connecting to " + String(ssid));
   strip.begin();
@@ -151,41 +178,46 @@ void setup() {
   delay(2000);
   WiFi.begin(ssid, password);
   WiFi.softAPdisconnect(true);
-  
+
   int i = 0;
   int count = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    if(i == 0) {
-         strip.setPixelColor(0, offColor);
-         i = 1;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    if (i == 0)
+    {
+      strip.setPixelColor(0, offColor);
+      i = 1;
     }
-    else{
-         strip.setPixelColor(0, blueColor);
-         i = 0;
+    else
+    {
+      strip.setPixelColor(0, blueColor);
+      i = 0;
 
-         //If we have failed many times restart
-         if(count++ > 20) {
-           ESP.restart();
-         }
+      // If we have failed many times restart
+      if (count++ > 20)
+      {
+        ESP.restart();
+      }
     }
     strip.show();
     delay(500);
     Serial.print(".");
   }
 
-  //Show the input id flashes = input id
+  // Show the input id flashes = input id
   strip.setPixelColor(0, offColor);
   strip.show();
-  delay(1000);  
-  for(int i = 0; i < inputID; i++) {
-      strip.setPixelColor(0, liveColor);
-      strip.show();
-      delay(200);
-      strip.setPixelColor(0, offColor);
-      strip.show();
-      delay(400);
+  delay(1000);
+  for (int i = 0; i < inputID; i++)
+  {
+    strip.setPixelColor(0, liveColor);
+    strip.show();
+    delay(200);
+    strip.setPixelColor(0, offColor);
+    strip.show();
+    delay(400);
   }
- 
+
   strip.setPixelColor(0, standbyColor);
   strip.show();
   delay(1000);
@@ -193,18 +225,27 @@ void setup() {
   udp.begin(INCOMING_PORT);
   subscribe();
   strip.setPixelColor(0, offColor);
-  if(showBlue) {strip.setPixelColor(1, blueColor);}else{strip.setPixelColor(1, offColor);}
+  if (showBlue)
+  {
+    strip.setPixelColor(1, blueColor);
+  }
+  else
+  {
+    strip.setPixelColor(1, offColor);
+  }
   Serial.print("My IP:");
   Serial.println(WiFi.localIP());
   Serial.println("Done");
 }
 
-void loop() {
-  //Check the wifi status
-  if(WiFi.status() != WL_CONNECTED) {
+void loop()
+{
+  // Check the wifi status
+  if (WiFi.status() != WL_CONNECTED)
+  {
     ESP.restart();
   }
 
-   receivePacket();
-   strip.show();
+  receivePacket();
+  strip.show();
 }
